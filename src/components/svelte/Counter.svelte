118c853a72ecc1959c6a96ed7e430f87d612dbf9
qfilip <kukolj.filip@gmail.com>
2024-11-09 19:55:13 +0100

    added new

---
+++ src/components/svelte/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let counter = 0;
+
+    function increment() {
+        counter = counter + 1
+    }
+</script>
+
+<div>{ counter }</div>
+<button on:click={increment}>Increment</button>
